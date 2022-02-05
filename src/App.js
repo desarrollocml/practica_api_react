35cf577ca891a8e78f71fe63a3c4d0089d16a4bd
desarrollocml <cristianmirandalamas@gmail.com>
2022-02-04 22:47:15 -0300

    commit 1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import { Quote } from "./componentes/Quote";
+
+const initialQuote = {
+  text: "text",
+  author: "autor",
+};
 
 function App() {
+  const [quote, setQuote] = useState(initialQuote);
+
+  const updateQuote = async ()=>{
+    const url = "https://www.breakingbadapi.com/api/quote/random"
+    const res = await fetch(url);
+    const newQuote = await res.json();
+    //console.log(newQuote)
+    
+  }
+
+  useEffect(() => {
+        updateQuote();
+    
+  }, []);
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <img
+        src="https://upload.wikimedia.org/wikipedia/commons/7/77/Breaking_Bad_logo.svg"
+        alt="logo"
+      />
+      <button>Get Another</button>
+      <Quote quote={quote}/>
     </div>
   );
 }
---
+++ src/componentes/Quote.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export const Quote = ({quote}) => {
+  return (
+    <p>
+      {quote.text} <br />
+      <span>- {quote.author}</span>
+    </p>
+  );
+};
